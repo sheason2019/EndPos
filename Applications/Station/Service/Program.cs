982be91e96d2fa578ed84649db8490eb6db02c81
sheason <811627877@qq.com>
2024-11-27 17:55:14 +0800

    feat: bootstrap

---
+++ Applications/Station/Service/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.MapGet("/hello-world", () => "Hello World").WithName("GetHelloWorld");
+
+app.Run();
